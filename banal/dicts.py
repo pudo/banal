ef632f159ab649020e7e0ed14a1f0f547f27a434
Friedrich Lindenberg <friedrich@pudo.org>
2017-10-25 19:06:40 +0200

    fix up clean_dict

--- banal/dicts.py
+++ banal/dicts.py
@@ -10,10 +10,11 @@ def is_mapping(obj):
 def clean_dict(data):
     """Remove None-valued keys from a dictionary, recursively."""
     if is_mapping(data):
+        out = {}
         for k, v in data.items():
-            if v is None:
-                data.pop(k)
-            data[k] = clean_dict(v)
+            if v is not None:
+                out[k] = clean_dict(v)
+        return out
     elif is_sequence(data):
         return [clean_dict(d) for d in data if d is not None]
     return data
