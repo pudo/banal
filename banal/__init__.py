9a654e33a735b442e6a5b1caf743f20e82583e10
Friedrich Lindenberg <friedrich@pudo.org>
2017-11-05 22:49:14 +0100

    include bool parsing

--- banal/__init__.py
+++ banal/__init__.py
@@ -2,7 +2,8 @@ from banal.lists import is_sequence, ensure_list, unique_list
 from banal.dicts import is_mapping, clean_dict
 from banal.filesystem import decode_path
 from banal.cache import hash_data
+from banal.bools import as_bool
 
-__all__ = [is_sequence, ensure_list, unique_list,
+__all__ = [is_sequence, ensure_list, unique_list, as_bool,
            is_mapping, clean_dict,
            decode_path, hash_data]
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='banal',
-    version='0.2.1',
+    version='0.3.0',
     description="Commons of banal micro-functions for Python.",
     long_description="",
     classifiers=[
