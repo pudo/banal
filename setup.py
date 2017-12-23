075a9b9fba6914021e0d2e458790110998d7606b
Friedrich Lindenberg <friedrich@pudo.org>
2017-12-23 14:24:31 +0100

    up version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='banal',
-    version='0.3.2',
+    version='0.3.3',
     description="Commons of banal micro-functions for Python.",
     long_description="",
     classifiers=[
