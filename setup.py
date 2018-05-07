0b73f5c5dfbfd9810c384298f82478b7999c77a0
Friedrich Lindenberg <friedrich@pudo.org>
2018-05-07 19:27:59 +0200

    release with license, refs #3.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='banal',
-    version='0.3.5',
+    version='0.3.6',
     description="Commons of banal micro-functions for Python.",
     long_description="",
     classifiers=[
