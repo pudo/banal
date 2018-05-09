940eb0174e60b056965966325f93be11d8d3e387
Friedrich Lindenberg <friedrich@pudo.org>
2018-05-09 11:29:17 +0200

    fix py3, release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='banal',
-    version='0.3.6',
+    version='0.3.7',
     description="Commons of banal micro-functions for Python.",
     long_description="",
     classifiers=[
