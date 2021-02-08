07b24b8da5317c43fb711ec5061521b5ac5b2dd1
Friedrich Lindenberg <friedrich@pudo.org>
2021-02-08 15:52:25 +0100

    Bump version: 1.0.1 → 1.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup  # type: ignore
 
 setup(
     name="banal",
-    version="1.0.1",
+    version="1.0.2",
     description="Commons of banal micro-functions for Python.",
     long_description="",
     classifiers=[
