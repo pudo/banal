dd41aaf84fb104cfc42333fb1ce89dd94bc5dc08
Friedrich Lindenberg <friedrich@pudo.org>
2021-02-08 17:35:37 +0100

    Bump version: 1.0.3 → 1.0.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 
 setup(
     name="banal",
-    version="1.0.3",
+    version="1.0.4",
     description="Commons of banal micro-functions for Python.",
     long_description=long_description,
     long_description_content_type="text/markdown",
